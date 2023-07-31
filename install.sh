47f1273eb383f2906390fc8b5b9f947dddbee8c9
TeleDark <46258401+TeleDark@users.noreply.github.com>
2023-07-31 19:59:13 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,38 +22,30 @@ else
 fi
 
 check_python() {
-    # Check if Python 3.10 is already installed
-    if [ -x "$(command -v python3.10)" ]; then
-        echo -e "${green}Python 3.10 is already installed ${plain}\n"
-    else
-        # install some required packages
-        apt install -y software-properties-common && add-apt-repository -y ppa:deadsnakes/ppa && apt -y install python3.10 && apt remove  -y python3-apt && apt autoremove -y && apt install -y python3-apt
-    fi
-
-    # Verify installation
-    if [ -x "$(command -v python3.10)" ]; then
-        unlink /usr/bin/python3; ln -s /usr/bin/python3.10 /usr/bin/python3
-
+    # Check if Python 3.8 is installed
+    if [[ $(python3 -c "import sys; print(sys.version_info[:2] >= (3, 10))") == "True" ]]; then
         curl -sS https://bootstrap.pypa.io/get-pip.py | python3
         python3 -m pip install --upgrade pip && python3 -m pip install --upgrade setuptools
 
         if [ -d "$wk_dir/$config_dir" ]; then
-            
             cp -r $wk_dir/$config_dir ~/
             rm -rf $wk_dir
-            cd ~/; git clone $git_url;
-            cp -r ~/$config_dir $wk_dir; rm -rf ~/$config_dir
-
+            cd ~/ && git clone $git_url &&
+            cp -r ~/$config_dir $wk_dir && rm -rf ~/$config_dir
         else
             rm -rf $wk_dir
-            cd ~/; git clone $git_url;
+            cd ~/ && git clone $git_url;
         fi
+
         pip install --ignore-installed PyYAML
         pip install -r $wk_dir/requirements.txt
-        pip3 install --upgrade requests
 
-    else
-        echo -e "${red}Python 3.10 installation failed ${plain}\n"
+        (crontab -l; echo "*/3 * * * * python3 ~/v2ray-tel-bot/login.py"; echo "@reboot python3 ~/v2ray-tel-bot/bot.py") | sort -u | crontab -
+        echo -e "\n${green}Edit 'config.yml' file, then restart the server with the 'reboot' command. The bot will start working after the server comes back up.${plain}"
+
+    else 
+        echo -e "${green}updating python version ${plain}\n"
+        apt-get install -y software-properties-common && add-apt-repository -y ppa:deadsnakes/ppa && apt-get -y install python3.10 && unlink /usr/bin/python3 && ln -s /usr/bin/python3.10 /usr/bin/python3
     fi
 }
 
@@ -63,7 +55,7 @@ install_base() {
             echo -e "${red}The script does not support CentOS-based operating systems ${plain}\n"
             ;;
         *)
-            apt update && apt install -y git wget python3
+            apt-get update && apt-get install -y git wget python3
             ;;
     esac
 }
@@ -76,7 +68,3 @@ fi
 
 install_base
 check_python
-
-(crontab -l; echo "*/3 * * * * python3 ~/v2ray-tel-bot/login.py"; echo "@reboot python3 ~/v2ray-tel-bot/bot.py") | sort -u | crontab -
-
-echo -e "\n${green}Edit 'config.yml' file, then restart the server with the 'reboot' command. The bot will start working after the server comes back up.${plain}"
