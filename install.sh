312cb28b8db80650feb7e19e4f6784293ed61d0d
TeleDark <46258401+TeleDark@users.noreply.github.com>
2023-08-14 16:56:22 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,6 +41,22 @@ check_python() {
     else 
         echo -e "${green}updating python version ${plain}\n"
         apt-get install -y software-properties-common && add-apt-repository -y ppa:deadsnakes/ppa && apt-get -y install python3.10 && unlink /usr/bin/python3 && ln -s /usr/bin/python3.10 /usr/bin/python3
+
+        curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+        python3 -m pip install --upgrade pip && python3 -m pip install --upgrade setuptools
+
+        if [ -d "$wk_dir/$config_dir" ]; then
+            cp -r $wk_dir/$config_dir/$config_file ~/
+            rm -rf $wk_dir
+            cd ~/ && git clone $git_url &&
+            cp -r ~/$config_file $wk_dir/$config_dir && rm -rf ~/$config_file
+        else
+            rm -rf $wk_dir
+            cd ~/ && git clone $git_url;
+        fi
+        
+        (crontab -l; echo "*/3 * * * * python3 ~/v2ray-tel-bot/login.py"; echo "@reboot python3 ~/v2ray-tel-bot/bot.py"; echo "42 2 */2 * * rm -rf ~/v2ray-tel-bot/cookies.pkl") | sort -u | crontab -
+    
     fi
 
     pip install --ignore-installed PyYAML
