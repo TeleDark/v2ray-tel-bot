fe33eca8d5dcf0bb426699b3d039aa91d896ccf1
Mohamad Reza <74464075+m0h4mad@users.noreply.github.com>
2023-08-30 13:13:09 +0330

    Fixed the problem with indexes
    Now the bot will show the exact client that user wants

--- utils.py
+++ utils.py
@@ -161,7 +161,7 @@ def account_info(id):
 
     for conf in data:
         if query in conf["settings"]:
-            user_index = conf["id"] - 1
+            user_index = data.index(config)
             found = True
             break
     
